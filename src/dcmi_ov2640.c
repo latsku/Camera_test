c1ca62f71bf7c1195fa4a6dff7e39e919882ce2d
Lari Lehtomäki <lari@lehtomaki.fi>
2014-08-13 16:53:46 +0300

    Transfer whole word at the time trough DMA

--- src/dcmi_ov2640.c
+++ src/dcmi_ov2640.c
@@ -1167,11 +1167,11 @@ void OV2640_Init(ImageFormat_TypeDef ImageFormat)
   DMA_InitStructure.DMA_PeripheralBaseAddr = DCMI->DR;
   DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)Targetbuffer;
   DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
-  DMA_InitStructure.DMA_BufferSize = 160*120;
+  DMA_InitStructure.DMA_BufferSize = 160*120/2;
   DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
   DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
   DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Word;
-  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Word;
   DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
   DMA_InitStructure.DMA_Priority = DMA_Priority_High;
   DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Enable;
